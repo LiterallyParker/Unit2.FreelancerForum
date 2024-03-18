397153951d30fdc1a14253999a7cc6b10377b020
LiterallyParker <parkernash2001@gmail.com>
2024-03-18 01:38:17 -0500

    FINAL Final product

--- script.js
+++ script.js
@@ -33,31 +33,57 @@ const OCCUPATIONS = [
     "Lawyer"
 ]
 const AVG_SPOT = document.getElementById('avg')
-const TABLE = document.getElementsByClassName('freelancers')[0]
+const TABLE = document.querySelector('table')
+
 class Freelancer {
+
     constructor() {
+
         this.name = NAMES[Math.floor(Math.random() * NAMES.length)];
+
         this.occupation = OCCUPATIONS[Math.floor(Math.random() * OCCUPATIONS.length)];
+
         this.price = String(Math.round(Math.random() * 100))
+
     }
 
     get_string() {
+
         return `<td class="freelancer">${this.name}</td><td class="freelancer">${this.occupation}</td><td class="freelancer">${this.price}</td>`
+
     }
+
 }
 
-const freelancers = []
-let total = 0
+const F1 = new Freelancer()
+const F2 = new Freelancer()
+const F3 = new Freelancer()
+const freelancers = [F1, F2, F3]
+
+for(let i = 0; i < freelancers.length; i++) {
+
+    TABLE.innerHTML += freelancers[i].get_string()
+
+}
+
+function get_avg() {
+
+    let total = freelancers.reduce((total, value) => total + Number(value.price), 0)
+    total = Math.round(total / freelancers.length)
+    return total
+
+}
+
 setInterval(() => {
+
     if(freelancers.length < 20) { 
+
         let freelancer = new Freelancer()
-        TABLE.innerHTML += freelancer.get_string()
         freelancers.push(freelancer)
-        total = freelancers.reduce((total, value) => total + Number(value.price), 0)
-        total = Math.round(total / freelancers.length)
-        AVG_SPOT.innerHTML = String(total)
+
+        TABLE.innerHTML += freelancer.get_string()
+        AVG_SPOT.innerHTML = String(get_avg())
+
     }
-}, 1000)
 
-
-
+}, 2000)
